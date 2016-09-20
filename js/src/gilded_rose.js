8b3d04d6690c4eceb5abdd7d479d3443b248b253
krihamilton <krihamilton@gumtree.com>
2016-09-20 20:22:49 +0100

    Add forth part of rafactoring changes

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -18,21 +18,25 @@ function GildedRose(name, sell_in, quality) {
   this.update_quality = function() {
     if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (this.quality > 0 && this.name != 'Sulfuras, Hand of Ragnaros') {
-          this.quality = this.quality - 1
+          this.decreaseQualityBy(1)
       }
     } else {
       if (this.quality < 50) {
-        this.quality = this.quality + 1
-        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.sell_in < 11 && this.quality < 50) {
-              this.increaseQualityBy(1);
-
-          }
-          if (this.sell_in < 6 && this.quality < 50) {
-              this.increaseQualityBy(1);
-          }
-        }
+        this.increaseQualityBy(1);
       }
+
+      if (this.name == 'Backstage passes to a TAFKAL80ETC concert'
+        && this.sell_in < 11
+        && this.quality < 50) {
+          this.increaseQualityBy(1);
+
+      }
+      if (this.name == 'Backstage passes to a TAFKAL80ETC concert'
+        && this.sell_in < 6
+        && this.quality < 50) {
+          this.increaseQualityBy(1);
+      }
+
     }
     if (this.name != 'Sulfuras, Hand of Ragnaros') {
       this.sell_in = this.sell_in - 1;
@@ -47,8 +51,9 @@ function GildedRose(name, sell_in, quality) {
       this.decreaseQualityBy(1);
     }
 
-    else if (this.name != 'Aged Brie'
-             && this.name == 'Backstage passes to a TAFKAL80ETC concert'
+
+
+    else if (this.name == 'Backstage passes to a TAFKAL80ETC concert'
              && this.sell_in < 0) {
       this.quality = this.quality - this.quality
     }
